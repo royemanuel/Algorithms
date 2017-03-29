387664c92618d68cb505ddf82cd432eaa81b37cb
Roy Emanuel <roy.emanuel@gmail.com>
2017-03-29 14:37:38 -0400

    First files

---
+++ Initials.java
@@ -0,0 +1,14 @@
+public class Initials{
+    public static void main(String[] args) {
+        //Prints "Hello, World" in the terminal window.
+        System.out.println("****      **      **  **********");
+        System.out.println("**  **    ***     **  **        ");
+        System.out.println("**    **  ****    **  **        ");
+        System.out.println("**    **  ** **   **  **        ");
+        System.out.println("**  **    **  **  **  **********");
+        System.out.println("****      **   ** **  **        ");
+        System.out.println("**  **    **    ****  **        ");
+        System.out.println("**   **   **     ***  **        ");
+        System.out.println("**    **  **      **  **********");
+    }
+}
